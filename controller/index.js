24d8dce9406d24e021c05aec94276510f7b72c81
DP3PO <williampferebee@gmail.com>
2021-03-04 22:33:12 -0500

    async worked
    got it to work

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,8 @@
 const CarModel = require('../model/index');
 
 module.exports = {
-        list: (req, res) => {
-                CarModel.find((err, car) => {
+        list: async (req, res) => {
+               await CarModel.find((err, car) => {
                     if (err) {
                         res.status(500).json({
                             messgae: "There was an error",
@@ -12,8 +12,8 @@ module.exports = {
                    return res.json(car);
                 })
         },
-        show: (req, res) => {
-            CarModel.find({"type.make": req.params.make}, (err, cars) => {
+        show: async(req, res) => {
+            await CarModel.find({"type.make": req.params.make}, (err, cars) => {
                 if (err) {
                     res.status(404).json({
                         message: "There was an error",
@@ -29,18 +29,18 @@ module.exports = {
                return res.json(cars);
             });
         },
-            create: (req, res) => {
-                let vehicle = new CarModel({
+            create: async (req, res) => {
+               let vehicle = new CarModel({
                     'type.make': req.body.make,
                     'type.model': req.body.model,
                     'color': req.body.color
                 });
-                vehicle.save((err, car) => {
+             await vehicle.save((err, car) => {
                     if (err) {
                         res.status(500).json({
                             message: " OOPSIES",
                             error: err
-                       });
+                        });
                     }
                     return res.status(201).json(car);
                 });
